7d04c9b11857f5104ab8b30df9cc4cc18dec3f42
Carlos Loaeza <carlosloaeza100598@gmail.com>
2021-05-04 21:46:29 -0500

    primera parte

--- src/java/control/Ahorcado.java
+++ src/java/control/Ahorcado.java
@@ -28,75 +28,77 @@ public class Ahorcado extends HttpServlet {
         try {
             inten = Integer.parseInt((String) request.getParameter("nintento"));
         } catch (NumberFormatException e) {
-            String palabra = (String) sesion.getAttribute("palabra");
-            String aciertos;
-            String errados;
-            if (palabra == null) {
-                Random oran = new Random();
-                palabra = PALABRAS[oran.nextInt(PALABRAS.length - 1)];
-                aciertos = "";
-                errados = "";
-                sesion.setAttribute("palabra", palabra);
-                sesion.setAttribute("aciertos", aciertos);
-                sesion.setAttribute("errados", errados);
-            } else {
-                aciertos = (String) sesion.getAttribute("aciertos");
-                errados = (String) sesion.getAttribute("errados");
-                String letra = request.getParameter("letra");
-                if(palabra.indexOf(letra)>=0){
-                    aciertos+=letra;
-                }else{
-                    errados+=letra;
-                }
-                sesion.setAttribute("aciertos",aciertos);
-                sesion.setAttribute("errados", errados);
-            }
-            
-            PrintWriter out = response.getWriter();
-            
-            try{
-                out.println("<html>");
-                out.println("<head>");
-                out.println("<title>Ahorcado</title>");
-                out.println("</head>");
-                out.println("<body>");
-                out.println("<center>");
-                out.println("<h2>Juego</h2>");
-                out.println("<h2>"+nombre+"</h2>");
-                out.println("<h3>Selecciona una letra</h3>");
-                boolean terminado = false;
-                out.println("<h2>");
-                for(int i=0; i<palabra.length();i++){
-                    String letra = palabra.substring(i,i+1);
-                    if(aciertos.indexOf(letra)>=0){
-                        out.println(""+letra);
-                    }else{
-                        out.println(""+"_");
-                        terminado = false;
-                    }
-                }
-                out.println("</h2>");
-                if(errados.length()<inten){
-                    out.println("<br/><br/><br/>");
-                    for(char car='A'; car<='Z'; car++){
-                        if(aciertos.indexOf(car)==-1 && errados.indexOf(car)==-1)
-                            out.println("<a href='Ahorcado?letra="+car+"'>"+car+"</a>");        
-                    }
-                    out.println("<br/><h2>"+"Oportunidades de errar: "+(inten-errados.length())+"</h2>");
-                }else{
-                    sesion.invalidate();
-                    out.println("<br/><h3>JUEGO TERMINADO</h3>");
-                    out.println("<br/><a href='index.jsp'>regresa</a>");
-                }
-                if(terminado){
-                    sesion.invalidate();
-                    out.println("<br/><h2>JUEGO TERMINADO</h2>");
-                    out.println("<br/><a href='index.jsp'>regresa</a>");
-                }
-            }catch(Exception eeee){
-                
-            }
         }
+
+        String palabra = (String) sesion.getAttribute("palabra");
+        String aciertos;
+        String errados;
+        if (palabra == null) {
+            Random oran = new Random();
+            palabra = PALABRAS[oran.nextInt(PALABRAS.length - 1)];
+            aciertos = "";
+            errados = "";
+            sesion.setAttribute("palabra", palabra);
+            sesion.setAttribute("aciertos", aciertos);
+            sesion.setAttribute("errados", errados);
+        } else {
+            aciertos = (String) sesion.getAttribute("aciertos");
+            errados = (String) sesion.getAttribute("errados");
+            String letra = request.getParameter("letra");
+            if (palabra.indexOf(letra) >= 0) 
+                aciertos += letra;
+             else 
+                errados += letra;
+            sesion.setAttribute("aciertos", aciertos);
+            sesion.setAttribute("errados", errados);
+        }
+
+        PrintWriter out = response.getWriter();
+
+        try {
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Ahorcado</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<center>");
+            out.println("<h2>Juego</h2>");
+            out.println("<h2>" + nombre + "</h2>");
+            out.println("<h3>Selecciona una letra</h3>");
+            boolean terminado = false;
+            out.println("<h2>");
+            for (int i = 0; i < palabra.length(); i++) {
+                String letra = palabra.substring(i, i + 1);
+                if (aciertos.indexOf(letra) >= 0) {
+                    out.println("" + letra);
+                } else {
+                    out.println("" + "_");
+                    terminado = false;
+                }
+            }
+            out.println("</h2>");
+            if (errados.length() < inten) {
+                out.println("<br/><br/><br/>");
+                for (char car = 'A'; car <= 'Z'; car++) {
+                    if (aciertos.indexOf(car) == -1 && errados.indexOf(car) == -1) {
+                        out.println("<a href='Ahorcado?letra=" + car + "'>" + car + "</a>");
+                    }
+                }
+                out.println("<br/><h2>" + "Oportunidades de errar: " + (inten - errados.length()) + "</h2>");
+            } else {
+                sesion.invalidate();
+                out.println("<br/><h3>JUEGO TERMINADO</h3>");
+                out.println("<br/><a href='index.jsp'>regresa</a>");
+            }
+            if (terminado) {
+                sesion.invalidate();
+                out.println("<br/><h2>JUEGO TERMINADO</h2>");
+                out.println("<br/><a href='index.jsp'>regresa</a>");
+            }
+        } catch (Exception eeee) {
+
+        }
+
     }
 
     @Override
